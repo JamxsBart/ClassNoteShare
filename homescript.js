44f9cb3db2bffdc952c2ae7dc1b3a44be3121d14
JamxsBart <140333958+JamxsBart@users.noreply.github.com>
2026-01-12 12:47:31 +0000

    StyleHomepageUpdate

---
+++ homescript.js
@@ -0,0 +1,9 @@
+  function scrollToModels2() {
+    var section2 = document.getElementById("section2");
+    section2.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  function scrollToModels3() {
+    var section1 = document.getElementById("section1");
+    section1.scrollIntoView({ behavior: 'smooth' });
+  }
